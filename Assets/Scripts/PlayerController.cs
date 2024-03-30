edd9b4d795ab84cc26180252c0252041ae62dd34
BMasterton <braonainmasterton@gmail.com>
2024-03-30 14:25:50 -0700

    Created Base Project
    Added:
    Player and animations
    Jumping and moving
    Backgrounds and tilesets with paletts
    added camera following and parallax movement
    Added future new tiles and more

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,39 +4,98 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // movement variables
-    //[SerializeField] Rigidbody2D rbody; 
-    //private float horizInput;
-    //private float moveSpeed = 450.0f;  // 4.5 * 100 since we're using Rigidbody physics to move
+    [SerializeField] private Rigidbody2D rbody;
+    [SerializeField] Animator anim;
 
-    // jump variables
-    //private float jumpHeight = 3.0f;
-    //private float jumpTime = 0.75f;
-    //private float initialJumpVelocity;
+    private float horizInput;           // store horizontal input for used in FixedUpdate()
+    private float moveSpeed = 450.0f;   // 4.5 * 100 newtons
 
+    private float jumpHeight = 3.0f;    // height of jump in units
+    private float jumpTime = 0.75f;     // time of jump in seconds
+    private float initialJumpVelocity;  // calculated jump velocity
 
-    // Start is called before the first frame update
-    void Start()
+    private bool isGrounded = false;    // true if player is grounded
+    [SerializeField] private Transform groundCheckPoint;    // draw a circle around this to check ground
+    [SerializeField] private LayerMask groundLayerMask;     // a layer for all things ground
+    private float groundCheckRadius = 0.3f;                 // radius of ground check circle
+
+    private int jumpMax = 2;            // # of jumps player can do without touching ground
+    private int jumpsAvailable = 0;     // current jumps available to player
+
+    private bool facingRight = true;    // true if facing right
+
+    private void Start()
     {
-        // given a desired jumpHeight and jumpTime, calculate gravity (same formulas as 3D)
-        //float timeToApex = jumpTime / 2.0f;
-        //float gravity = (-2 * jumpHeight) / Mathf.Pow(timeToApex, 2);
+        // calculate gravity using gravity formula
+        float timeToApex = jumpTime / 2.0f;
+        float gravity = (-2 * jumpHeight) / Mathf.Pow(timeToApex, 2);
+        Debug.Log("gravity:" + gravity);
 
-        // calculate jump velocity (upward motion)
-        //initialJumpVelocity = Mathf.Sqrt(jumpHeight * -2 * gravity);      
+        // adjust gravity scale of player based on gravity required for jumpHeight & jumpTime
+        rbody.gravityScale = gravity / Physics2D.gravity.y;
+
+        // calculate jump velocity req'd for jumpHeight & jumpTime
+        initialJumpVelocity = Mathf.Sqrt(jumpHeight * -2 * gravity);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // read (and store) horizontal input
-        //horizInput = Input.GetAxis("Horizontal");        
+        // read & store horizontal input
+        horizInput = Input.GetAxis("Horizontal");
+
+        // determine if player is running (for animator param)
+        bool isRunning = horizInput > 0.01 || horizInput < -0.01;
+        anim.SetBool("isRunning", isRunning);   // notify animator
+
+        // determine if player is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayerMask) && rbody.velocity.y < 0.01;
+        anim.SetBool("isGrounded", isGrounded); // notify animator
+
+        // reset jumps if grounded
+        if (isGrounded)
+        {
+            jumpsAvailable = jumpMax;
+        }
+
+        // if jump is triggered & available - go for it
+        if (Input.GetButtonDown("Jump") && jumpsAvailable > 0)
+        {
+            Jump();
+        }
+
+        // Flip player if appropriate
+        if ((facingRight && horizInput < -0.01) ||
+            (!facingRight && horizInput > 0.01))
+        {
+            Flip();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // draw the ground check sphere in the Scene
+        Gizmos.DrawSphere(groundCheckPoint.position, groundCheckRadius);
     }
 
     private void FixedUpdate()
     {
-        // We're moving via Rigidbody physics (setting velocity directly) so we need to use FixedUpdate
-        //float xVel = horizInput * moveSpeed * Time.deltaTime;   // determine x velocity
-        //rbody.velocity = new Vector2(xVel, rbody.velocity.y);   // set new x velocity, maintain existing y velocity
+        // move the player (use horizontal input, but maintain existing y velocity)
+        float xVel = horizInput * moveSpeed * Time.deltaTime;
+        rbody.velocity = new Vector2(xVel, rbody.velocity.y);
+    }
+
+    void Jump()
+    {
+        // tell the player to jump
+        rbody.velocity = new Vector2(rbody.velocity.x, initialJumpVelocity);
+        jumpsAvailable--;
+        anim.SetTrigger("jump");    // notify animator
+    }
+
+    void Flip()
+    {
+        // flip the direction the player is facing
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up, 180);
     }
 }
