cc6034d8f907af7446a798c4854ab069232e9499
BMasterton <braonainmasterton@gmail.com>
2024-04-01 18:21:49 -0700

    bullet fix
    made it so bullets get destroyed after time and upon hitting certain enemies or platforms or basically anything. then made it so bullets are uneffected by gravity

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -16,15 +16,31 @@ public class Bullet : MonoBehaviour
         
     }
 
-    //for now this works to kill enemies but i need to give them health and then do health checks 
-    private void OnCollisionEnter2D(Collision2D collision)
+    IEnumerator destoryBullet(Collision2D collision)
     {
-
-        //Destroy(gameObject);
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Untagged" || collision.gameObject.tag == "platform")
         {
             //Destroy(collision.gameObject);
             Destroy(gameObject);
         }
+
+       
+        yield return new WaitForSeconds(0.55f);
+        //might need to make it so they get destroyed when touching anything
+        Destroy(gameObject);
+    }
+
+        //for now this works to kill enemies but i need to give them health and then do health checks 
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+
+        StartCoroutine(destoryBullet(collision));
+        //Destroy(gameObject);
+        //if (collision.gameObject.tag == "Enemy")
+        //{
+          
+        //    //Destroy(collision.gameObject);
+        //    Destroy(gameObject);
+        //}
     }
 }
