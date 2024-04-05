b163b46a73a4bc87f86a13115d8613165fb03e84
Braonain Masterton <99563726+BMasterton@users.noreply.github.com>
2024-04-04 17:57:04 -0700

    sequence script

--- Assets/Scripts/TimedPlatformSequenceManager.cs
+++ Assets/Scripts/TimedPlatformSequenceManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TimedPlatformSequenceManager : MonoBehaviour
 {
+    GameObject[] timedPlatforms;
     // Start is called before the first frame update
     void Start()
     {
