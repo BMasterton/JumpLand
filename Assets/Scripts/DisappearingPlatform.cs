c6bdb8c892e0d8d9ff8324b3bdc257ff746721f9
Braonain Masterton <99563726+BMasterton@users.noreply.github.com>
2024-04-04 17:56:35 -0700

    trying to add timed platforms

--- Assets/Scripts/DisappearingPlatform.cs
+++ Assets/Scripts/DisappearingPlatform.cs
@@ -5,71 +5,7 @@ using UnityEngine;
 public class DisappearingPlatform : MonoBehaviour
 {
 
-    //// Variables to control timing
-    //private bool playerOnPlatform = false;
-    //private float timePlayerEntered;
-    //private bool platformActive = true;
-
-    //// Variables for timing intervals
-    //public float disappearDelay = 2f; // Time it takes for the platform to disappear after player lands on it
-    //public float appearDelay = 5f;    // Time it takes for the platform to reappear after disappearing
-
-    //// Reference to the platform's SpriteRenderer
-    //private SpriteRenderer spriteRenderer;
-    //private BoxCollider2D boxCollider;
-
-    //private void Start()
-    //{
-    //    // Get the SpriteRenderer component
-    //    spriteRenderer = GetComponent<SpriteRenderer>();
-    //    boxCollider = GetComponent<BoxCollider2D>();
-    //}
-
-    //private void Update()
-    //{
-    //    if (playerOnPlatform && platformActive)
-    //    {
-    //        // If the player is on the platform and it's still active
-    //        if (Time.time - timePlayerEntered > disappearDelay)
-    //        {
-    //            // If enough time has passed, make the platform disappear
-    //            spriteRenderer.enabled = false;
-    //            boxCollider.enabled = false;
-    //            platformActive = false;
-    //            // Start timer for reappearing
-    //            Invoke("MakePlatformAppear", appearDelay);
-    //        }
-    //    }
-    //}
-
-    //private void MakePlatformAppear()
-    //{
-    //    // Make the platform reappear
-    //    spriteRenderer.enabled = true;
-    //    boxCollider.enabled = true;
-    //    platformActive = true;
-    //    // Reset player on platform flag
-    //    playerOnPlatform = false;
-    //}
-
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Player"))
-    //    {
-    //        // If player lands on the platform, set the playerOnPlatform flag and record the time
-    //        playerOnPlatform = true;
-    //        timePlayerEntered = Time.time;
-    //    }
-    //}
-
-    //private void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Player"))
-    //    {
-    //        // If player leaves the platform, reset the playerOnPlatform flag
-    //        playerOnPlatform = false;
-    //    }
-    //}
+ 
     [SerializeField] GameObject platform;
     private SpriteRenderer spriteRenderer;
     private BoxCollider2D boxCollider;
@@ -99,16 +35,14 @@ public class DisappearingPlatform : MonoBehaviour
     {
 
         //Transform position = platform.transform;
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
 
         spriteRenderer.enabled = false;
         boxCollider.enabled = false;
-        yield return new WaitForSeconds(2);//wait x amount of seconds
+        yield return new WaitForSeconds(1);//wait x amount of seconds
         spriteRenderer.enabled = true;
         boxCollider.enabled = true;
-        //platform.SetActive(true);
-        //_ = Instantiate(platform) as GameObject;
-        //platform.transform.position = position.position;
+       
 
         Debug.Log("Exited Deactivate");
     }
@@ -116,17 +50,12 @@ public class DisappearingPlatform : MonoBehaviour
     IEnumerator deactivate()
     {
        
-        //Transform position = platform.transform;
-        //Debug.Log("Entered deactivate");
         yield return new WaitForSeconds(2);//wait x amount of seconds
        spriteRenderer.enabled = false;
         boxCollider.enabled = false;
         yield return new WaitForSeconds(2);//wait x amount of seconds
         spriteRenderer.enabled = true;
         boxCollider.enabled = true;
-        //platform.SetActive(false);
-        //Destroy(platform);
-
-        //StartCoroutine(activate());
+    
     }
 }
--- Assets/Scripts/TimedPlatform.cs
+++ Assets/Scripts/TimedPlatform.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class TimedPlatform : MonoBehaviour
 {
+    [SerializeField] GameObject platform;
+   // private SpriteRenderer spriteRenderer;
+    //private BoxCollider2D boxCollider;
+
+    private int timer = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+        //boxCollider = GetComponent<BoxCollider2D>();
+
+        //GameObject child = platform.transform.GetChild(0).gameObject;
+        //child.spriteRenderer.enabled = false;
+
+        platform.GetComponentInChildren<SpriteRenderer>
+
+        spriteRenderer = platform.GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        StartCoroutine(flash(timer));
+    }
+
+
+    IEnumerator flash(int timer)
+    {
+
+        //Transform position = platform.transform;
+        yield return new WaitForSeconds(timer);
+
+        spriteRenderer.enabled = false;
+        boxCollider.enabled = false;
+        yield return new WaitForSeconds(2);//wait x amount of seconds
+        spriteRenderer.enabled = true;
+        boxCollider.enabled = true;
+
+        flash(timer);
     }
 
     // Update is called once per frame
